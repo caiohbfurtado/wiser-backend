51e9a83baee105b6653a8ec9174e10284503570b
Caio Barutti <caiohbfurtado@gmail.com>
2021-02-20 20:56:17 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f68e198cf6c41c99313980bf3a09019f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6acdf834-86c3-49e7-8ab9-253476b84586'
+};
